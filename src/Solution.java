7a3501d5db0c3bd3c2d4c338bf066b949d311bf2
sergeyesp <sergey_esp@i.ua>
2021-06-16 10:25:36 +0300

    Added file

---
+++ src/Solution.java
@@ -0,0 +1,45 @@
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+/* 
+Шифровка
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+    	int key = 31415926;
+    	List<Integer> password = new ArrayList<>();
+    	while (key > 0) {
+    		int l = key % 10;
+    		password.add(l);
+    		key /= 10;
+    	}
+
+        FileInputStream fis = new FileInputStream("/home/sergey/temp2.txt");
+        FileOutputStream fos = new FileOutputStream("/home/sergey/temp3.txt");
+        
+        int i = 0;
+        
+        if (args[0].equals("-e")) {
+            while (fis.available() > 0) {
+            	if (i == password.size()) i = 0;
+            	fos.write(fis.read() + password.get(i));
+            	i++;
+            }
+        } else if (args[0].equals("-d")) {
+            while (fis.available() > 0) {
+            	if (i == password.size()) i = 0;
+            	fos.write(fis.read() - password.get(i));
+            	i++;
+            }
+        } else System.out.println("Неверный ключ, используйте -e или -d");
+        
+        fis.close();
+        fos.close();
+    }
+
+}
